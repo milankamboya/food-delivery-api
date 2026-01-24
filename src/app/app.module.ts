1a3aeee58e00c82b52d8d9c017f0f03222b34b6b
Milan-Kamboya <4450462+milankamboya@users.noreply.github.com>
2026-01-24 23:54:14 +0530

    feat: initial structure with modules for different functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { MealModule } from 'src/meal/meal.module';
+import { OrderModule } from 'src/order/order.module';
+import { RestaurantModule } from 'src/restaurant/restaurant.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [AuthModule, MealModule, OrderModule, RestaurantModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
