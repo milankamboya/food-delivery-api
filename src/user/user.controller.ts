55d0fc7e5e28edadee4b17c0681834c59f8ae9a6
Milan-Kamboya <4450462+milankamboya@users.noreply.github.com>
2026-01-29 00:36:10 +0530

    cleanup: removed extra route as of now

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 
@@ -6,23 +6,8 @@ import { CurrentUser } from '../common/decorators/current-user.decorator';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
   @Get('me')
   getProfile(@CurrentUser() user: { id: string }) {
     return this.userService.findOne(user.id);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(id);
   }
 }
